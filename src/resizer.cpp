4a0f5fcd3f406010a782d98a8716b253c9c830f9
Gavin1937 <gyh1837771475gyh@gmail.com>
2022-05-02 01:39:13 -0700

    allow iqdb to handle png,gif,bmp file

--- src/resizer.cpp
+++ src/resizer.cpp
@@ -28,36 +28,57 @@
 
 namespace iqdb {
 
-enum image_types { IMG_UNKNOWN, IMG_JPEG };
+enum image_types { IMG_UNKNOWN, IMG_JPEG, IMG_PNG, IMG_GIF, IMG_BMP };
 
 image_types get_image_info(const unsigned char *data, size_t length) {
-  if (length >= 2 && data[0] == 0xff && data[1] == 0xd8) {
+  if (length >= 3 && memcmp(data, "\xff\xd8\xff", 3) == 0) {
     return IMG_JPEG;
+  } else if (length >= 4 && memcmp(data, "\x89\x50\x4e\x47", 4) == 0) {
+    return IMG_PNG;
+  } else if (length >= 3 && memcmp(data, "\x47\x49\x46", 3) == 0) {
+    return IMG_GIF;
+  } else if (length >= 2 && memcmp(data, "\x42\x4d", 2) == 0) {
+    return IMG_BMP;
   } else {
     return IMG_UNKNOWN;
   }
 }
 
+RawImage get_raw_image(image_types type, size_t len, const unsigned char *data)
+{
+  switch (type)
+  {
+  case IMG_JPEG:
+    return RawImage(gdImageCreateFromJpegPtr((int)len, const_cast<unsigned char *>(data)), &gdImageDestroy);
+  case IMG_PNG:
+    return RawImage(gdImageCreateFromPngPtr((int)len, const_cast<unsigned char *>(data)), &gdImageDestroy);
+  case IMG_GIF:
+    return RawImage(gdImageCreateFromGifPtr((int)len, const_cast<unsigned char *>(data)), &gdImageDestroy);
+  case IMG_BMP:
+    return RawImage(gdImageCreateFromBmpPtr((int)len, const_cast<unsigned char *>(data)), &gdImageDestroy);
+  
+  default:
+    throw image_error("Unsupported image format.");
+  }
+}
+
 RawImage resize_image_data(const unsigned char *data, size_t len, unsigned int thu_x, unsigned int thu_y) {
   auto type = get_image_info(data, len);
-
-  if (type != IMG_JPEG)
-    throw image_error("Unsupported image format.");
-
+  
   RawImage thu(gdImageCreateTrueColor(thu_x, thu_y), &gdImageDestroy);
   if (!thu)
     throw image_error("Out of memory.");
-
-  RawImage img(gdImageCreateFromJpegPtr((int)len, const_cast<unsigned char *>(data)), &gdImageDestroy);
+  
+  RawImage img = get_raw_image(type, len, data);
   if (!img)
     throw image_error("Could not read image.");
-
+  
   if ((unsigned int)img->sx == thu_x && (unsigned int)img->sy == thu_y && gdImageTrueColor(img))
     return img;
-
+  
   gdImageCopyResampled(thu.get(), img.get(), 0, 0, 0, 0, thu_x, thu_y, img->sx, img->sy);
   DEBUG("Resized {} x {} to {} x {}.\n", img->sx, img->sy, thu_x, thu_y);
-
+  
   return thu;
 }
 
